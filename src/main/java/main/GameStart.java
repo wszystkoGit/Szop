2f9da7b57c474bce57df0a255810e6353a86c786
Jarek <fletsky@gmail.com>
2021-05-20 16:52:22 +0200

    Adding JavaFx GUI

--- src/main/java/main/GameStart.java
+++ src/main/java/main/GameStart.java
@@ -3,10 +3,14 @@ package main;
 
 import view.TextUI;
 
+
+import static javafx.application.Application.launch;
+
 public class GameStart {
 
 
     public static void main(String[] args){
+        launch(view.javafx.Gui.class);
         TextUI testUI = new TextUI();
 
         testUI.test();
